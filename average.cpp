b74ee244150c320848aed7fa9a3c95a0c690d9e2
Kartikeya Mishra <128405226+ka-rtikkk@users.noreply.github.com>
2025-10-01 22:26:31 +0530

    Add files via upload

---
+++ average.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    float m;
+    cout<<"enter two numbers:"<<endl;
+    cin>>a>>b;
+    m=(a+b)/2.0;
+    cout<<"the average is "<<m<<endl;
+    return 0;
+
+}
